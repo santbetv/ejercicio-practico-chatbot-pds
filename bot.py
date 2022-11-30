6771615ce65e2a6e6903b55e6ab6a3a30336063c
Sebastian Gomez Quintero <65258610+sebastiangomezq10@users.noreply.github.com>
2022-11-30 00:23:30 +0000

    Se realiza los cambios para implementar el saludo del bot #6

--- bot.py
+++ bot.py
@@ -1,18 +1,27 @@
-#########################################################
 from config import bot
 import config
 from time import sleep
+import logic
 import re
 #########################################################
 # Aquí vendrá la implementación de la lógica del bot
+
+
 @bot.message_handler(commands=['start'])
 def on_command_start(message):
     bot.send_chat_action(message.chat.id, 'typing')
     sleep(1)
+    saludo = ""
+    print(message.from_user.id)
+    if logic.check_admin(message.from_user.id):
+        saludo = logic.get_welcome_messageAdmin(bot.get_me())
+    else:
+        saludo = logic.get_welcome_messageUser(bot.get_me())
     bot.send_message(
         message.chat.id,
-        "Hola, soy un \U0001F916 de restaurante, ¿cómo estás?",
-        parse_mode="Markdown") 
+        saludo,        
+        parse_mode="Markdown")
+
 
 #########################################################
 if __name__ == '__main__':
---
+++ logic.py
@@ -0,0 +1,25 @@
+def get_welcome_messageAdmin(bot_data):
+    response = (
+        f"Hola, soy *{bot_data.first_name}* \n"
+        "¡Estoy aquí para ayudarte, podrás: \n"
+        "- Gestionar(adicionar, listar y Cambiar estado) los platos del restaurante.!\n"
+        "- Gestionar los pedidos del restaurante"
+    )
+    return response
+
+
+def get_welcome_messageUser(bot_data):
+    response = (
+        f"Hola, soy *{bot_data.first_name}* "
+        "¡Estoy aquí para ayudarte, podrás: \n"
+        "- Consultar los platos del restaurante \n"
+        "- Gestionar tu carrito de compras\n"
+        "- Revisar tu historial de compras de los últimos 20 platos."
+    )
+    return response
+
+
+def check_admin(user_id):
+    admins = [ 1441882294]
+    return user_id in admins    
+    # return False
